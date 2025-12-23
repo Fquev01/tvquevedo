efcc58d2b7859bd4ae25fba3ee51707b50994684
Francois <franky_385@hotmail.com>
2025-12-22 20:01:46 -0600

    cambio de vite para show.html

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/tvquevedo/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        show: resolve(__dirname, "show.html"),
+      },
+    },
+  },
 });
